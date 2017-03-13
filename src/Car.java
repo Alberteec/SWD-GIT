a8f4576fd2d168da18194725ecada0b240cfe70b
Jonathan Hansen <jona072b@stud.kea.dk>
2017-03-13 12:22:03 +0000

    Hej Alberte

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Jonathan on 13-03-2017.
+ */
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
